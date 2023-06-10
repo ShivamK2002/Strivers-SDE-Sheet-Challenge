aa4246bb9c08f4fbf8883a4aa70894d44254d83b
Shivam Kawale <ultimateengineer2002@gmail.com>
2023-06-10 17:51:02 +0530

    n/2 majority element optimum add

--- n/2_majorityElement.cpp
+++ n/2_majorityElement.cpp
@@ -23,3 +23,34 @@ int findMajorityElement(int arr[], int n)
     }
     return -1;
 }
+
+// morre's voting algo - optimum
+
+int findMajorityElement(int arr[], int n)
+{
+    int count = 0;
+    int ele;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (count == 0)
+        {
+            count++;
+            ele = arr[i];
+        }
+
+        if (arr[i] == ele)
+            count++;
+        else
+            count--;
+    }
+    int freq = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == ele)
+            freq++;
+    }
+    if (freq > n / 2)
+        return ele;
+    return -1;
+}

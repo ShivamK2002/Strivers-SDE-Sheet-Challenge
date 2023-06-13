7725a3c324575eeb866190fd92b2b8dd67e9722c
Shivam Kawale <ultimateengineer2002@gmail.com>
2023-06-13 19:02:32 +0530

    test

--- merge2sortedArrays.cpp
+++ merge2sortedArrays.cpp
@@ -13,6 +13,7 @@ vector<int> ninjaAndSortedArrays(vector<int> &arr1, vector<int> &arr2, int m, in
     // 		}
     // 	}
     // 	sort(arr1.begin(),arr1.end());
+
     // 	sort(arr2.begin(),arr2.end());
     // 	int j=m;
     // 	for(int i=0; i<n; i++){
